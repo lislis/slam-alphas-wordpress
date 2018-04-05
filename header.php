2c230b06df8b9df78466e51d6756a43640bd3f74
lislis <lislis@users.noreply.github.com>
2018-04-05 23:36:09 +0200

    put font loading in explicitly (#4)

--- header.php
+++ header.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="<?php bloginfo( 'charset' ); ?>">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet"> 
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
